7ac4032c5a1928f86ee79f91c5b839146120e59a
USER <mmsgod0427@naver.com>
2025-08-21 23:31:23 +0900

    커밋 추가

--- src/main/java/com/bs/boot/myboardrest/config/config/SwaggerConfig.java
+++ src/main/java/com/bs/boot/myboardrest/config/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.bs.boot.myboardrest.config.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,5 +27,27 @@ public class SwaggerConfig {
                 .pathsToMatch("/**")
                 .packagesToScan("com.airoom.airoom")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI jwtOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP) // 인증 방식이 HTTP(S) 헤더임을 명시
+                .scheme("bearer")               // Authorization: Bearer <token> 형태
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)   // 헤더 이름
+                .name("Authorization");         // 사용할 헤더 이름
+
+        // 컴포넌트에 담아서 나중에 참조해서 사용이 가능하다.
+        Components components = new Components()
+                .addSecuritySchemes("bearer", securityScheme);
+
+        // 전역 보안 요구사항 : 모든 API에 bearer 스키마를 적용 -> JWT를 적용한다.
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearer");
+
+        return new OpenAPI()
+                .components(components)
+                .addSecurityItem(securityRequirement);
+
     }
 }
