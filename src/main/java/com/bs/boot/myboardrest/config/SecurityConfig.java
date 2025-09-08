7e092c15f8c6ad540092f9814daad889ee92fcfc
MyeongSooJang <mmsgod0427@gmail.com>
2025-09-08 23:34:15 +0900

    RestController

--- src/main/java/com/bs/boot/myboardrest/config/SecurityConfig.java
+++ src/main/java/com/bs/boot/myboardrest/config/SecurityConfig.java
@@ -1,4 +1,14 @@
 package com.bs.boot.myboardrest.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
+
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
 }
