5a46320398fa169189bbec46a251f220201e683b
MyeongSooJang <mmsgod0427@gmail.com>
2025-09-13 22:51:07 +0900

    cors

--- src/main/java/com/bs/boot/myboardrest/board/controller/BoardController.java
+++ src/main/java/com/bs/boot/myboardrest/board/controller/BoardController.java
@@ -1,8 +1,10 @@
 package com.bs.boot.myboardrest.board.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Controller
 public class BoardController {
 
 
